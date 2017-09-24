836e78161825f8f26350f4532f914bfbe7b7c3ed
Maksym <maksymilian.makuch7@gmail.com>
2017-09-24 22:55:41 +0200

    Pole trójkąta

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+var a = 10,
+    h = 15,
+	triangleArea = a*h/2;
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+alert('Pole trójkąta wynosi ' + triangleArea);
