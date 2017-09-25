5d24f0ea49166314fe0a91d43788746c9ac23ffc
Maksym <maksymilian.makuch7@gmail.com>
2017-09-25 18:33:39 +0200

    z indexu usunięta treść, js poprawiony zgodnie z zaleceniem na - dodana funkcja prompt dla danych

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-var a = 10,
-    h = 15,
+var a = prompt('Wpisz długość boku trójkąta '),
+    h = prompt('Wpisz wartość wysokości w trójkącie '),
 	triangleArea = a*h/2;
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
 alert('Pole trójkąta wynosi ' + triangleArea);
